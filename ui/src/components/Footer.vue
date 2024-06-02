33e49c8fc0612c35f46975d0625d7e870949152e
Ariel <arielqhou@gmail.com>
2024-06-01 22:26:05 -0400

    Init repo.

---
+++ ui/src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="grid">
+    <ul class="credits">
+      <li>
+        <a href="http://github.com/diiiefiend/cocktailist-ui" target="_blank">
+          repo
+        </a>
+      </li>
+      <li>
+        <a href="http://projects.lukehaas.me/css-loaders" target="_blank">
+          loader
+        </a>
+        |
+        <a href="https://thenounproject.com/" target="_blank">
+          vector
+        </a>
+      </li>
+    </ul>
+    <div class="quicklinks">
+      <a href="javascript:void(0);" class="toplink scrollToTop">back to top?</a>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue {
+  // @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/styles/components/footer.scss";
+</style>
---
+++ ui/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="grid">
+    <div id="logo">
+      <a href="/" @mouseover="hovered = true" @mouseleave="hovered = false">
+        <img alt="cocktailist" :src="[hovered ? imgHover : imgStandard]" />
+      </a>
+    </div>
+    <nav>
+      <li><router-link to="/">Cocktails</router-link></li>
+      <li><router-link to="/feed">Activity</router-link></li>
+      <li><router-link to="/lists">Lists</router-link></li>
+      <li><router-link to="/data">Data</router-link></li>
+      <li><router-link to="/login">Login</router-link></li>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {
+  imgStandard = "/images/deco-logo.png";
+  imgHover = "/images/deco-logo-hover.png";
+
+  data() {
+    return {
+      hovered: false
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/styles/components/header.scss";
+</style>
